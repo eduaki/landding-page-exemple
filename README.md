bca8f6d7ee42a16d85bc1dcf479669a5e0a4408b
Eduardo Alves <edu.alves2003@gmail.com>
2024-05-25 19:20:52 -0300

    inicializing project(all of assets has been imported to the project)

---
+++ README.md
@@ -0,0 +1,18 @@
+# A Exemple Landing Page
+
+This is a simple exemple of a landingPage. I get this design in figma Comunnity.
+
+I using reactJs with Vite and typeScript.
+
+the buttons on app is not working, because the design on figma not have other
+pages.
+
+- view the
+  [figma design](<"https://www.figma.com/design/GrK07yvJXVkywde6qweI3J/Saas-Landing-Pages-(Community)?node-id=0-416&t=pnD9DbNfz9IE77oi-0">)
+
+### Running the project
+
+- clone repo `git clone <repoLink>`
+- run `npm install`
+
+All of this code has maked by [@eduaki]("github.com/eduaki")
---
+++ src/components/imagesImports.ts
@@ -0,0 +1,49 @@
+import logo from "/src/assets/logo.png";
+import mainBanner from "/src/assets/mainBanner.png";
+import secondBanner from "/src/assets/secondBanner.png";
+// IMPORTS icons svg
+import arrowRight from "/src/assets/icons/arrow-right.svg";
+import coinGroup from "/src/assets/icons/coinGroup.svg";
+import play from "/src/assets/icons/Play.svg";
+import check from "/src/assets/icons/Subtract.svg";
+// IMPORTS icons png
+import command from "/src/assets/icons/command.png";
+import fiveStars from "/src/assets/icons/fiveStars.png";
+import fourStars from "/src/assets/icons/fourStars.png";
+import pieChart from "/src/assets/icons/pieChart.png";
+import heartSign from "/src/assets/icons/heartSign.png";
+// IMPORTS photos
+import photo1 from "/src/assets/photos/photo1.png";
+import photo2 from "/src/assets/photos/photo2.png";
+import photo3 from "/src/assets/photos/photo3.png";
+import photo4 from "/src/assets/photos/photo4.png";
+// IMPORTS covers
+import cover1 from "/src/assets/covers/cover1.png";
+import cover2 from "/src/assets/covers/cover2.png";
+import cover3 from "/src/assets/covers/cover3.png";
+
+// EXPORTS IMAGES
+export const Logo = logo;
+export const MainBanner = mainBanner;
+export const SecondBanner = secondBanner;
+
+export const Photo1 = photo1;
+export const Photo2 = photo2;
+export const Photo3 = photo3;
+export const Photo4 = photo4;
+
+export const Cover1 = cover1;
+export const Cover2 = cover2;
+export const Cover3 = cover3;
+
+/// EXPORTS ICONS
+export const ArrowRight = arrowRight;
+export const CoinGroup = coinGroup;
+export const Play = play;
+export const Check = check;
+
+export const Command = command;
+export const FiveStars = fiveStars;
+export const FourStars = fourStars;
+export const PieChart = pieChart;
+export const HeartSign = heartSign;
